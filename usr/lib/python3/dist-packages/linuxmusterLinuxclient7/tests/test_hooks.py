16db20b12760d67cd8285579aa72de60a38b2404
Dorian Zedler <dorian@itsblue.de>
2022-08-17 09:10:48 +0200

    Tests: test remote hook execution

--- usr/lib/python3/dist-packages/linuxmusterLinuxclient7/tests/test_hooks.py
+++ usr/lib/python3/dist-packages/linuxmusterLinuxclient7/tests/test_hooks.py
@@ -1,26 +1,27 @@
+from email.mime import base
+from pathlib import Path
 from unittest import mock
 from .. import hooks, fileHelper
 import pytest, os
 
 @mock.patch("subprocess.call")
+@mock.patch("linuxmusterLinuxclient7.hooks.config.network")
+@mock.patch("linuxmusterLinuxclient7.hooks.user.readAttributes")
+@mock.patch("linuxmusterLinuxclient7.hooks.computer.readAttributes")
+@mock.patch("linuxmusterLinuxclient7.hooks.shares.getLocalSysvolPath")
 @mock.patch("linuxmusterLinuxclient7.hooks.constants.etcBaseDir", "/tmp/hooks")
 @pytest.mark.parametrize("hook", [hooks.Type.Boot, hooks.Type.Shutdown, hooks.Type.LoginAsRoot, hooks.Type.Login, hooks.Type.SessionStarted, hooks.Type.LogoutAsRoot, hooks.Type.LoginLogoutAsRoot])
-def test_runHook(mockSubprocessCall,hook):
-    try:
-        os.mkdir("/tmp/hooks")
-    except FileExistsError:
-        pass
+def test_runHook(mockSharesGetLocalSysvolPath, mockComputerAttributes, mockUserAttributes, mockConfigNetwork, mockSubprocessCall, hook):
 
-    try:
-        os.mkdir(f"/tmp/hooks/on{hook.name}.d")
-    except FileExistsError:
-        pass
+    # mock ennvironment
+    mockConfigNetwork.return_value = (True, {"domain": "linuxmuster.lan"})
+    mockUserAttributes.return_value = (True, {"sophomorixSchoolname": "default-school"})
+    mockComputerAttributes.return_value = (True, {"sophomorixSchoolname": "default-school"})
+    mockSharesGetLocalSysvolPath.return_value = (True, "/tmp/sysvol")
 
-    for i in range(2):
-        with open(f"/tmp/hooks/on{hook.name}.d/script-{i}", "w") as file:
-            file.write("#!/bin/bash\necho \"test-{i}\"")
-        os.chmod(f"/tmp/hooks/on{hook.name}.d/script-{i}", 0o777)
-        
+    hookScripts = _createLocalHookScripts(hook)
+    hookScripts = hookScripts + _createRemoteHookScripts(hook)
+
     hooks.runHook(hook)
 
     calls = []
@@ -29,10 +30,11 @@ def test_runHook(mockSubprocessCall,hook):
         if not firstArg in calls:
             calls.append(firstArg)
 
-    for i in range(2):
-        assert f"/tmp/hooks/on{hook.name}.d/script-{i}" in calls
+    for script in hookScripts:
+        assert script in calls
 
     fileHelper.deleteDirectory("/tmp/hooks")
+    fileHelper.deleteDirectory("/tmp/sysvol")
 
 @pytest.mark.parametrize("hook", [hooks.Type.Boot, hooks.Type.Shutdown, hooks.Type.LoginAsRoot, hooks.Type.Login, hooks.Type.SessionStarted, hooks.Type.LogoutAsRoot, hooks.Type.LoginLogoutAsRoot])
 def test_getLocalHookScript(hook):
@@ -64,3 +66,48 @@ def test_shouldHooksBeExecuted(mockSetupIsSetup, mockComputerIsInAD, mockUserUse
     mockComputerIsInAD.return_value = True
     mockUserIsUserInAD.return_value = False
     assert not hooks.shouldHooksBeExecuted()
+
+
+# --------------------
+# - Helper functions -
+# --------------------
+
+def _createLocalHookScripts(hook, basedir="/tmp/hooks"):
+    try:
+        os.mkdir(basedir)
+    except FileExistsError:
+        pass
+
+    try:
+        os.mkdir(f"{basedir}/on{hook.name}.d")
+    except FileExistsError:
+        pass
+
+    createdFiles = []
+    for i in range(2):
+        thisFile = f"{basedir}/on{hook.name}.d/script-{i}"
+        with open(thisFile, "w") as file:
+            file.write("#!/bin/bash\necho \"test-{i}\"")
+        os.chmod(thisFile, 0o777)
+        createdFiles.append(thisFile)
+
+    return createdFiles
+
+def _createRemoteHookScripts(hook, sysvolPath="/tmp/sysvol", domain="linuxmuster.lan", school="default-school"):
+    if not hook in hooks.remoteScriptNames:
+        return []
+
+    hookScriptPathTemplate = "{0}/{1}/scripts/{2}/{3}/linux/{4}".format(sysvolPath, domain, school, "{}", hooks.remoteScriptNames[hook])
+
+    createdFiles = []
+    for hookKind in ["lmn", "custom"]:
+        hookScriptPath = hookScriptPathTemplate.format(hookKind)
+        hookDir = os.path.dirname(hookScriptPath)
+        Path(hookDir).mkdir(parents=True, exist_ok=True)
+
+        with open(hookScriptPath, "w") as file:
+            file.write("#!/bin/bash\necho \"test-{i}\"")
+        os.chmod(hookScriptPath, 0o777)
+        createdFiles.append(hookScriptPath)
+
+    return createdFiles
