3c669ff1e43f17195c50bf93a7dadba33ddbef06
Dorian Zedler <dorian@itsblue.de>
2022-08-19 11:33:55 +0200

    Tests: improove tests for printers.uninstallAllPrintersOfUser

--- usr/lib/python3/dist-packages/linuxmusterLinuxclient7/tests/test_printers.py
+++ usr/lib/python3/dist-packages/linuxmusterLinuxclient7/tests/test_printers.py
@@ -36,17 +36,48 @@ def test_installPrinter(mockUserUsername, mockUserIsRoot, mockSubprocessCall):
     assert _getCallsTo(mockSubprocessCall, "sudo")[-1] == ['sudo', '/usr/share/linuxmuster-linuxclient7/scripts/sudoTools', 'install-printer', '--path', 'ipp://linuxmuster.lan/printers/printer1', '--name', 'printer2']
 
 @mock.patch("subprocess.call")
+@mock.patch("linuxmusterLinuxclient7.hooks.user.isRoot")
+@mock.patch("linuxmusterLinuxclient7.hooks.user.username")
+def test_installPrinterError(mockUserUsername, mockUserIsRoot, mockSubprocessCall):
+    mockSubprocessCall.return_value = 124
+    mockUserIsRoot.return_value = True
+    mockUserUsername.return_value = "user1"
+
+    assert not printers.installPrinter("ipp://linuxmuster.lan/printers/printer1")
+
+    mockSubprocessCall.return_value = 1
+    assert not printers.installPrinter("ipp://linuxmuster.lan/printers/printer1")
+
+
+@mock.patch("subprocess.call")
 @mock.patch("subprocess.run")
 def test_uninstallAllPrintersOfUser(mockSubprocessRun, mockSubprocessCall):
     mockSubprocessCall.return_value = 0
     lpstatStdout = """printer printer1 is idle.  enabled since Sat 02 Jul 2022 06:07:39 PM CEST
-printer printer2 is idle.  enabled since Sat 09 Jul 2022 08:04:43 PM CEST"""
+printer printer2 is idle.  enabled since Sat 09 Jul 2022 08:04:43 PM CEST
+invalid"""
     mockSubprocessRun.return_value = CompletedProcess(args=["lpstat", "-U", "user1", "-p"], returncode=0, stdout=lpstatStdout)
 
     assert printers.uninstallAllPrintersOfUser("user1")
     assert _getCallsTo(mockSubprocessRun, "lpstat")[-1] == "lpstat -U user1 -p"
     assert _getCallsTo(mockSubprocessCall, "timeout") == [["timeout", "10", "lpadmin", "-x", "printer1"], ["timeout", "10", "lpadmin", "-x", "printer2"]]
 
+    mockSubprocessRun.return_value = CompletedProcess(args=["lpstat", "-U", "user1", "-p"], returncode=1)
+    assert printers.uninstallAllPrintersOfUser("user1")
+
+@mock.patch("subprocess.call")
+@mock.patch("subprocess.run")
+def test_uninstallAllPrintersOfUser(mockSubprocessRun, mockSubprocessCall):
+    lpstatStdout = """printer printer1 is idle.  enabled since Sat 02 Jul 2022 06:07:39 PM CEST
+printer printer2 is idle.  enabled since Sat 09 Jul 2022 08:04:43 PM CEST
+invalid"""
+    mockSubprocessRun.return_value = CompletedProcess(args=["lpstat", "-U", "user1", "-p"], returncode=0, stdout=lpstatStdout)
+    mockSubprocessCall.return_value = 1
+    assert not printers.uninstallAllPrintersOfUser("user1")
+
+    mockSubprocessRun.return_value = CompletedProcess(args=["lpstat", "-U", "user1", "-p"], returncode=0, stdout=lpstatStdout)
+    mockSubprocessCall.return_value = 124
+    assert not printers.uninstallAllPrintersOfUser("user1")
 # --------------------
 # - Helper functions -
 # --------------------
