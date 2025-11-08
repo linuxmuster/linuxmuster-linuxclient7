3698d2ba4835a330245b91404c22fb755b1800a5
Dorian Zedler <dorian@itsblue.de>
2025-11-08 22:03:04 +0100

    Chore: remove unneeded code

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,6 @@ todo_include_todos = False
 
 html_theme = 'sphinx_rtd_theme'
 html_logo = '_static/logo.png'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 html_sidebars = {
     '**': [
